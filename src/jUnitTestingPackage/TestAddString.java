3e908133861715847856750e2af0ffc9a135249d
Hardik <Hardik@10.12.45.87>
2022-11-17 00:21:18 +0700

    Jtest

---
+++ src/jUnitTestingPackage/TestAddString.java
@@ -0,0 +1,14 @@
+package jUnitTestingPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAddString {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
